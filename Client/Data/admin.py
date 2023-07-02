94748d35f7c120ca968e2eae4361c7825a5f50a0
RoundedOrange <RoundedOrange@outlook.com>
2023-07-02 18:34:30 +0800

    enable basic admin page

--- Client/Data/admin.py
+++ Client/Data/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from Data.models import User,Dataset,Model,Device,Task
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Dataset)
+admin.site.register(Model)
+admin.site.register(Device)
+admin.site.register(Task)
