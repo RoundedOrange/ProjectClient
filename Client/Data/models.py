7b1f11dc4197a47ecedf3007ab5451db306d8093
RoundedOrange <RoundedOrange@outlook.com>
2023-07-04 00:19:13 +0800

    change database

--- Client/Data/models.py
+++ Client/Data/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     user_id = models.IntegerField(primary_key=True)
-    nickname = models.CharField(max_length=10,null=False)
+    nickname = models.CharField(max_length=10,null=False,unique=True)
+    real_name = models.CharField(max_length=10,null=False)
     telephone = models.CharField(max_length=10)
     birth_date = models.DateField(null=True)
     is_administrator = models.BooleanField(default=False)
@@ -13,17 +14,19 @@ class User(models.Model):
     password = models.CharField(max_length=256)
     email = models.CharField(max_length=20,null=True)
     gender = models.BooleanField(null=True)
+    balance = models.IntegerField(default=0)
 
 class Dataset(models.Model):
     dataset_id = models.IntegerField(primary_key=True)
     size = models.IntegerField()
     description = models.TextField(max_length=100,default="")
-    uploader = models.ForeignKey('User',on_delete=models.CASCADE)
 
-class Model(models.Model):
-    model_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=10)
-    description = models.TextField(max_length=100,default="")
+class User_Dataset(models.Model):
+    user = models.ForeignKey('User',on_delete=models.CASCADE)
+    Dataset = models.ForeignKey('Dataset',on_delete=models.CASCADE)
+    money = models.IntegerField()
+    begin_date = models.DateField(null=False)
+    end_date = models.DateField(null=False)
 
 class Device(models.Model):
     device_id = models.IntegerField(primary_key=True)
@@ -37,9 +40,10 @@ class Device(models.Model):
     OS = models.CharField(max_length=1,choices=OS_CHOICES,null=False)
     CPU = models.CharField(max_length=20,null=True)
     STATUS_CHOICES = (
-        ('0','Normal'),
-        ('1','Offline'),
-        ('2','Error'),
+        ('0','Running'),
+        ('1','Occupied'),
+        ('2','Vacant'),
+        ('3','Offline'),
     )
     status = models.CharField(max_length=1,choices=STATUS_CHOICES,default='0')
     RAM = models.IntegerField()
@@ -48,7 +52,7 @@ class Device(models.Model):
     can_run_cuda = models.BooleanField(default=False)
     description = models.TextField(max_length=100,default="")
     core_num = models.IntegerField(default=1)
-    bandwidth = models.IntegerField()
+    max_bandwidth = models.IntegerField()
 
 class Task(models.Model):
     task_id = models.IntegerField(primary_key=True)
@@ -57,5 +61,4 @@ class Task(models.Model):
     accuracy = models.DecimalField(max_digits=5,decimal_places=4,default=0)
     publisher = models.ForeignKey('User',on_delete=models.CASCADE)
     dataset = models.ForeignKey('Dataset',on_delete=models.CASCADE)
-    model = models.ForeignKey('Model',on_delete=models.CASCADE)
     device = models.ForeignKey('Device',on_delete=models.CASCADE)
