159ee30d3eaa42104c240191fbf6b66055689891
RoundedOrange <RoundedOrange@outlook.com>
2023-07-04 09:12:04 +0800

    register function implemented

--- Client/Login/forms.py
+++ Client/Login/forms.py
@@ -5,3 +5,10 @@ class LoginForm(forms.Form):
     nickname = forms.CharField(label="用户名",max_length=128,widget=forms.TextInput(attrs={'class':'input-material','placeholder':'请输入用户名','autofocus':''}))
     password = forms.CharField(label="密码",max_length=256,widget=forms.PasswordInput(attrs={'class':'input-material','placeholder':'请输入密码'}))
     captcha = CaptchaField(label='验证码')
+
+class RegisterForm(forms.Form):
+    nickname = forms.CharField(label="用户名",max_length=128,widget=forms.TextInput(attrs={'class':'input-material','placeholder':'请输入用户名','autofocus':''}))
+    password1 = forms.CharField(label="密码",max_length=256,widget=forms.PasswordInput(attrs={'class':'input-material','placeholder':'请输入密码'}))
+    password2 = forms.CharField(label="确认密码",max_length=256,widget=forms.PasswordInput(attrs={'class':'input-material','placeholder':'再次输入密码'}))
+    telephone = forms.CharField(label="手机号",max_length=11,widget=forms.TextInput(attrs={'class':'input-material','placeholder':'请输入11位手机号'}))
+    captcha = CaptchaField(label='验证码')
--- Client/Login/views.py
+++ Client/Login/views.py
@@ -10,7 +10,7 @@ def hash_code(s,salt='DataWrangler'):
 # Create your views here.
 def login(request):
     if request.session.get('is_login',None):
-        return redirect('/')
+        return redirect('/dashboard/')
     if request.method == 'POST':
         login_form = forms.LoginForm(request.POST)
         message = '请检查填写的内容！'
@@ -27,7 +27,7 @@ def login(request):
                 #request.session['is_login'] = True
                 #request.session['nickname'] = user.nickname
                 #request.session['user_id'] = user.user_id
-                return redirect('/')
+                return redirect('/dashboard/')
             else:
                 message = '密码不正确！'
                 return render(request,'login.html',locals())
@@ -36,7 +36,38 @@ def login(request):
     login_form = forms.LoginForm()
     return render(request,'login.html',locals())
 def register(request):
-    return render(request,'register.html')
+    if request.session.get('is_login',None):
+        return redirect('/dashboard/')
+    if request.method == 'POST':
+        register_form = forms.RegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():
+            nickname = register_form.cleaned_data.get('nickname')
+            password1 = register_form.cleaned_data.get('password1')
+            password2 = register_form.cleaned_data.get('password2')
+            telephone = register_form.cleaned_data.get('telephone')
+            if password1 != password2:
+                message = "两次输入的密码不同！"
+                return render(request,'register.html',locals())
+            else:
+                same_name_user= models.User.objects.filter(nickname=nickname)
+                if same_name_user:
+                    message = "用户名已经存在！"
+                    return render(request,'register.html',locals())
+                same_telephone_user = models.User.objects.filter(telephone=telephone)
+                if same_telephone_user:
+                    message = "手机号已经被注册！"
+                    return render(request,'register.html',locals())
+                new_user = models.User()
+                new_user.nickname = nickname
+                new_user.password = hash_code(password1)
+                new_user.telephone = telephone
+                #new_user.save()
+                return redirect('/login/')
+        else:
+            return render(request,'register.html',locals())
+    register_form = forms.RegisterForm()
+    return render(request,'register.html',locals())
 def logout(request):
     if not request.session.get('is_login',None):
         return redirect('/login/')
