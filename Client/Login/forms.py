17fdc6354c8293dfd4cc24d0aecdf10ac09a78b2
orange <100190293+cloudsaber@users.noreply.github.com>
2023-07-08 08:56:43 +0800

    qc commits

--- Client/Login/forms.py
+++ Client/Login/forms.py
@@ -10,5 +10,5 @@ class RegisterForm(forms.Form):
     nickname = forms.CharField(label="用户名",max_length=128,widget=forms.TextInput(attrs={'class':'input-material','placeholder':'请输入用户名','autofocus':''}))
     password1 = forms.CharField(label="密码",max_length=256,widget=forms.PasswordInput(attrs={'class':'input-material','placeholder':'请输入密码'}))
     password2 = forms.CharField(label="确认密码",max_length=256,widget=forms.PasswordInput(attrs={'class':'input-material','placeholder':'再次输入密码'}))
-    telephone = forms.CharField(label="手机号",max_length=11,widget=forms.TextInput(attrs={'class':'input-material','placeholder':'请输入11位手机号'}))
-    captcha = CaptchaField(label='验证码')
+    telephone = forms.CharField(label="手机号",max_length=20,widget=forms.TextInput(attrs={'class':'input-material','placeholder':'请输入11位手机号'}))
+    captcha = forms.CharField(label="验证码",max_length=6,widget=forms.TextInput(attrs={'class':'input-material','placeholder':'请输入6位验证码'}))
--- Client/Login/urls.py
+++ Client/Login/urls.py
@@ -3,4 +3,6 @@ from Login import views
 urlpatterns = [
     path('',views.login),
     path('register',views.register),
+    path('send-captcha', views.send_captcha, name='send_captcha'),  # 新增发送验证码的 URL
+    path('logout',views.logout),
 ]
